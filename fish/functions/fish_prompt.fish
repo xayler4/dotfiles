a9710d67bcf98c1b38dabd92ee42697ac4217482
Andrea Pastore <andrea.pastore04@outlook.com>
2024-08-03 12:31:19 +0200

    fixed fish shell

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,5 +1,9 @@
 function fish_prompt
-	echo -n (set_color $fish_color_login)$USER'@'(prompt_hostname) (set_color $fish_color_cwd)(prompt_pwd)(set_color $fish_color_git_status)(fish_git_prompt) (set_color $fish_color_suffix)'󰽰 '
+    set cwd (set_color $fish_color_cwd)(prompt_pwd)
+    set git_prompt (set_color $fish_color_git_status)(fish_git_prompt)
+    set combined "$cwd$git_prompt"
+
+	echo -n (set_color $fish_color_login)$USER'󰁥 '(prompt_hostname) $combined (set_color $fish_color_suffix)'󰽰 '
 end
 
 funcsave fish_prompt
