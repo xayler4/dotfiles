5033f6e0bce17675ae11a709ea2ca0bf7b725774
Andrea Pastore <andrea.pastore04@outlook.com>
2024-08-03 14:58:22 +0200

    fish shell colors changed

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,9 +1,9 @@
 function fish_prompt
-    set cwd (set_color $fish_color_cwd)(prompt_pwd)
+    set cwd (set_color $fish_color_cwd)(prompt_pwd --full-length-dirs=2)
     set git_prompt (set_color $fish_color_git_status)(fish_git_prompt)
     set combined "$cwd$git_prompt"
 
-	echo -n (set_color $fish_color_login)$USER'󰁥 '(prompt_hostname) $combined (set_color $fish_color_suffix)'󰽰 '
+	echo -n (set_color $fish_color_login)$USER'@'(prompt_hostname)(set_color $fish_color_separator)':'$combined(set_color $fish_color_suffix)'$ '
 end
 
 funcsave fish_prompt
